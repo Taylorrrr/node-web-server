6a293103214e28e2e20fee359b1804b3bc241640
Taylor Hao <40265153+Taylorrrr@users.noreply.github.com>
2019-01-28 22:59:01 +1300

    Add projects page

--- server.js
+++ server.js
@@ -48,6 +48,12 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Project Page'
+    });
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'unable to handle request'
